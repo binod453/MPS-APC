95d042a3acd3d01f26307d3632be052c6ac412af
binod453 <35481943+binod453@users.noreply.github.com>
2020-08-06 15:44:06 +0530

    Add files via upload

---
+++ APC Codebase/MPS-APC/Authorization/src/main/java/org/Authorization/App.java
@@ -0,0 +1,13 @@
+package org.Authorization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
